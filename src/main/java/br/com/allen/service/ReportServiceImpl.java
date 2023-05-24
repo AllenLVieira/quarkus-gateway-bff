d7937985955c9351da0d339770519915a1557ca0
AllenLVieira <allenvieira96@gmail.com>
2023-05-23 21:28:51 -0300

    fix: add @RestClient

--- src/main/java/br/com/allen/service/ReportServiceImpl.java
+++ src/main/java/br/com/allen/service/ReportServiceImpl.java
@@ -5,6 +5,7 @@ import br.com.allen.dto.OpportunityDTO;
 import br.com.allen.utils.CSVUtils;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 public class ReportServiceImpl implements ReportService {
 
     @Inject
+    @RestClient
     ReportRestClient reportRestClient;
 
     @Override
