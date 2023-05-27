7845fd5a94ef1659d0bac2d1b21b05ff19c86d0d
AllenLVieira <allenvieira96@gmail.com>
2023-05-27 00:27:24 -0300

    feat: add logs to CSVUtils

--- src/main/java/br/com/allen/utils/CSVUtils.java
+++ src/main/java/br/com/allen/utils/CSVUtils.java
@@ -4,6 +4,8 @@ import br.com.allen.dto.OpportunityDTO;
 import br.com.allen.enums.HeaderEnum;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -13,6 +15,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CSVUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CSVUtils.class);
+
+    private CSVUtils() {
+        throw new IllegalStateException("Classe Utilitária.");
+    }
 
     public static ByteArrayInputStream opportunitiesToCSV(List<OpportunityDTO> opportunities) {
         final CSVFormat format = CSVFormat.Builder.create().setHeader(HeaderEnum.class).build();
@@ -25,8 +33,11 @@ public class CSVUtils {
             }
 
             csvPrinter.flush();
+
+            LOGGER.info("CSV gerado com sucesso.");
             return new ByteArrayInputStream(output.toByteArray());
         } catch (IOException e) {
+            LOGGER.error("Falha ao importar dados para o CSV: {}", e.getMessage(), e);
             throw new RuntimeException("Falha ao importar dados para CSV: " + e.getMessage(), e);
         }
     }
