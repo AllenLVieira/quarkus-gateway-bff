37716dd13c8e9df881801d5b028c5a7230030a4c
AllenLVieira <allenvieira96@gmail.com>
2023-05-27 00:35:35 -0300

    feat: add logs to services

--- src/main/java/br/com/allen/service/ProposalServiceImpl.java
+++ src/main/java/br/com/allen/service/ProposalServiceImpl.java
@@ -6,9 +6,13 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class ProposalServiceImpl implements ProposalService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProposalServiceImpl.class);
 
     @Inject
     @RestClient
@@ -16,16 +20,25 @@ public class ProposalServiceImpl implements ProposalService {
 
     @Override
     public ProposalDetailsDTO getProposalDetailsById(long proposalId) {
-        return proposalRestClient.getProposalDetailsById(proposalId);
+        LOGGER.info("Recebendo detalhes da proposta de ID: {}", proposalId);
+        ProposalDetailsDTO proposalDetailsById = proposalRestClient.getProposalDetailsById(proposalId);
+        LOGGER.info("Dados de proposta recebidos: {}", proposalDetailsById);
+        return proposalDetailsById;
     }
 
     @Override
     public Response createProposal(ProposalDetailsDTO proposalDetails) {
-        return proposalRestClient.createProposal(proposalDetails);
+        LOGGER.info("Criando proposta: {}", proposalDetails);
+        Response response = proposalRestClient.createProposal(proposalDetails);
+        LOGGER.info("Resposta da criação de proposta: {}", response);
+        return response;
     }
 
     @Override
     public Response removeProposal(long proposalId) {
-        return proposalRestClient.removeProposal(proposalId);
+        LOGGER.info("Deletando proposta de ID: {}", proposalId);
+        Response response = proposalRestClient.removeProposal(proposalId);
+        LOGGER.info("Proposta de ID: {} removida. {}", proposalId, response);
+        return response;
     }
 }
